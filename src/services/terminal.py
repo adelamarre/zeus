de008afc4212696a615289df02873a35310e2f50
Your Name <you@example.com>
2021-02-28 15:20:17 +0100

    Add specific value color in terminal appendTemplate

--- src/services/terminal.py
+++ src/services/terminal.py
@@ -21,8 +21,13 @@ class Terminal:
     def append(self, text: str = ''):
         self.lines.append(text)
     
-    def appendTemplate(self, template: str, data, valueColor: str =  Fore.CYAN, textColor: str = Fore.LIGHTWHITE_EX + Style.DIM):
+    def appendTemplate(self, template: str, data, valueColor: str =  Fore.CYAN, textColor: str = Fore.LIGHTWHITE_EX + Style.DIM, valueColors: dict = None):
         template = re.sub(r'(\{.*?\})', Fore.RESET + Style.RESET_ALL + valueColor + '\\1' + textColor, template, re.LOCALE)
+        if valueColors:
+            for valueName in valueColors:
+                template = re.sub(r'(\{%s.*?\})' % valueName, Fore.RESET + Style.RESET_ALL + valueColors[valueName] + '\\1' + textColor, template, re.LOCALE)
+        
+            
         self.append((textColor + template + Fore.RESET + Style.RESET_ALL).format(**data))
 
     def appendSeparator(self, color: str = Fore.BLUE):
